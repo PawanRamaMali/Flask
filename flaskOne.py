30902a3735efa78859d101e38479127ee218c9c5
Pawan Rama Mali <prm@outlook.in>
2021-11-07 13:26:23 +0530

    String variables

--- flaskOne.py
+++ flaskOne.py
@@ -19,11 +19,29 @@ def query_strings(greetings="hello"):
     return '<h1> the greeting is : {0} </h1>'.format(query_val)
 
 
-# Add variables in route
+# Define variables in route
 @app.route('/user')
 @app.route('/user/<name>')
 def no_query_strings(name='Tina'):
     return '<h1> hello there {} </h1>'.format(name)
+
+
+# String variables
+@app.route('/text/<string:name>')
+def text_display(name="hello"):
+    return '<h1> the string is {}'.format(name) + '</h1>'
+
+
+# Number variables
+@app.route('/add/<int:num1>/<int:num2>')
+def add_two_numbers(num1=0, num2=0):
+    return '<h1> the addition of numbers is {}'.format(num1+num2) + '</h1>'
+
+
+# Float variables
+@app.route('/sum/<float:num1>/<float:num2>')
+def sum_two_numbers(num1, num2):
+    return '<h1> the sum of numbers is {}'.format(num1+num2) + '</h1>'
 
 
 if __name__ == '__main__':
