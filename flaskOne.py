33847a8f175211cc710111dcb6fef15e135b4d76
Pawan Rama Mali <prm@outlook.in>
2021-11-07 19:46:39 +0530

    Jinja MACROS

--- flaskOne.py
+++ flaskOne.py
@@ -96,5 +96,17 @@ def filter_data():
                            film='a christmas carol')
 
 
+# Jinja MACROS
+@app.route('/macros')
+def jinja_macros():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('using_macros.html', movies=movies_dict)
+
 if __name__ == '__main__':
     app.run(debug=True)
